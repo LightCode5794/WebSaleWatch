a45c78aa7ad99fa2747d136505a655c1fe8baf76
LightCode5794 <trantronghoang5794@gmail.com>
2022-10-27 02:46:49 +0700

    update front-end

--- UI-FrontEnd/js/index.js
+++ UI-FrontEnd/js/index.js
@@ -3,9 +3,22 @@
 function openSidebar() {
   document.getElementById("mySidebar").style.display = "block";
   document.getElementById("myOverlay").style.display = "block";
+  var header = document.getElementById("header");
+  if(header.className === "header"){
+    header.style.position = "fixed";
+    header.style.left = "0";
+    header.style.right = "0";
+    header.className += " animate-hide-to-top";
+   
+  }
 }
 
 function closeSidebar() {
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById("myOverlay").style.display = "none";
+  var header = document.getElementById("header");
+  if(header.className === "header animate-hide-to-top"){
+    header.className = "header";
+    
+  }
 }
