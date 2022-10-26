e4471f892ed00e2c5ee47deedfc8f64126d22167
LightCode5794 <trantronghoang5794@gmail.com>
2022-10-26 20:31:37 +0700

    update side bar basic

---
+++ UI-FrontEnd/js/index.js
@@ -0,0 +1,11 @@
+
+// event for click sidebar
+function openSidebar() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSidebar() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
