3f0beb9890924729d470d4c3fd5d8ae715fd03a9
LightCode5794 <trantronghoang5794@gmail.com>
2022-10-28 02:41:00 +0700

    add animation for sidebar and header

--- UI-FrontEnd/js/index.js
+++ UI-FrontEnd/js/index.js
@@ -1,34 +1,71 @@
 
 // event for click sidebar
 function openSidebar() {
-  document.getElementById("mySidebar").style.display = "block";
+
   document.getElementById("myOverlay").style.display = "block";
+  var mySidebar = document.getElementById("mySidebar");
+  mySidebar.style.display = "block";
+  // appear the sidebar
+  mySidebar.animate({
+    opacity: 1,
+    left: 0,
+
+  },
+
+    {
+      duration: 300,
+      fill: 'forwards'
+    })
+
+
+  // disapear the header
   var header = document.getElementById("header");
-  if(header.className === "header"){
-    header.style.position = "fixed";
-    header.style.left = "0";
-    header.style.right = "0";
-    header.className += " animate-hide-to-top";
-   
-  }
+  header.animate({
+    opacity: 0,
+    transform: 'translateY(-100%)',
+  },
+    {
+      duration: 300,
+      fill: 'forwards'
+    })
 }
 
 function closeSidebar() {
-  document.getElementById("mySidebar").style.display = "none";
+
   document.getElementById("myOverlay").style.display = "none";
   var header = document.getElementById("header");
-  if(header.className === "header animate-hide-to-top"){
-    header.className = "header";
-  }
+
+  // disapear the sidebar
+  var mySidebar = document.getElementById("mySidebar");
+  mySidebar.animate(
+    {
+      left: '-40%',
+      opacity: 0,
+      // transform: 'translateX(-100%)',
+    },
+
+    {
+      duration: 300,
+      fill: 'forwards'
+    })
+  
+  // re-appear the sidebar
+  header.animate({
+    opacity: 1,
+    transform: 'translateY(0)',
+  },
+    {
+      duration: 300,
+      fill: 'forwards'
+    })
 }
 
 
-  let iconSearch = document.querySelector(".search__toggle");
-  iconSearch.addEventListener( "click", () => iconSearch.classList.toggle("toggled") )
+
+let iconSearch = document.querySelector(".search__toggle");
+iconSearch.addEventListener("click", () => iconSearch.classList.toggle("toggled"))
 
 
-  // let button = document.querySelector(".hamburger__toggle");
-  // button.addEventListener( "click", () => button.classList.toggle("toggled") );
 
 
 
